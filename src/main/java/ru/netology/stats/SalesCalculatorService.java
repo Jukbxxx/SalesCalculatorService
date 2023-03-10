4de4fdd3e6d01b3876c2a449c98f3c9423760bfa
Maria Sarkisyan <mariia.sarkisyan@ya.ru>
2023-03-10 21:07:18 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/SalesCalculatorService.java
@@ -0,0 +1,65 @@
+package ru.netology.stats;
+
+public class SalesCalculatorService {
+    public int getMinSales(long[] sales) {
+        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                minMonth = i; // запомним его как минимальный
+            }
+        }
+
+        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int getSalesSum(long[] sales) {
+        int sum = 0; // сумма всех продаж
+        for (long amount : sales) { // amount - количество продаж в рамках одного месяца
+            sum += amount; // суммируем количество продаж за все месяцы
+        }
+        return sum;
+    }
+
+    public int getAverageSales(long[] sales) {
+        int sum = getSalesSum(sales); // берем сумму всех продаж, получаемую при помощи нашего метода выше
+        int average = sum / sales.length; // делим сумму на количество месяцев в массиве данных
+        return average;
+    }
+
+    public int getMaxSales(long[] sales) {
+        int maxMonth = 0; // номер месяца с максимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) { // значит, в рассматриваемом i-м месяце продаж больше
+                maxMonth = i; // запомним его как максимальный
+            }
+        }
+
+        return maxMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int getMonthsBelowAverage(long[] sales) {
+        int belowAverageNumber = 0; // количество месяцев с продажами ниже среднего
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < getAverageSales(sales)) { // берем среднее значение, получаемое при помощи нашего метода выше
+                belowAverageNumber = belowAverageNumber + 1; // если продажи в рассматриваемом месяца были ниже среднего, прибавляем 1
+            }
+        }
+        return belowAverageNumber;
+    }
+
+    public int getMonthAboveAverage(long[] sales) {
+        int aboveAverageNumber = 0; // количество месяцев с продажами выше среднего
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < getAverageSales(sales)) { // логика по аналогии с методом выше
+                aboveAverageNumber = aboveAverageNumber + 1;
+            }
+        }
+        return aboveAverageNumber;
+    }
+
+}
+
---
+++ src/test/java/ru/netology/stats/SalesCalculatorServiceTest.java
@@ -0,0 +1,82 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SalesCalculatorServiceTest {
+
+    @Test
+    public void shouldFindMinSalesBetweenEnds() {
+        SalesCalculatorService service = new SalesCalculatorService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedMinMonth = 9;
+        int actualMinMonth = service.getMinSales(sales);
+
+        Assertions.assertEquals(expectedMinMonth, actualMinMonth);
+    }
+
+    @Test
+    public void shouldCalcSum() {
+        SalesCalculatorService service = new SalesCalculatorService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedSum = 180;
+        int actualSum = service.getSalesSum(sales);
+
+        Assertions.assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void shouldCalcAverage() {
+        SalesCalculatorService service = new SalesCalculatorService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedAverage = 15;
+        int actualAverage = service.getAverageSales(sales);
+
+        Assertions.assertEquals(expectedAverage, actualAverage);
+
+    }
+
+    @Test
+    public void shouldFindMaxBetweenEnds() {
+        SalesCalculatorService service = new SalesCalculatorService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedMaxMonth = 8;
+        int actualMaxMonth = service.getMaxSales(sales);
+
+        Assertions.assertEquals(expectedMaxMonth, actualMaxMonth);
+    }
+
+    @Test
+    public void shouldFindBelowAverageNumber() {
+        SalesCalculatorService service = new SalesCalculatorService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedBelowAverageNumber = 5;
+        int actualBelowAverageNumber = service.getMonthsBelowAverage(sales);
+
+        Assertions.assertEquals(expectedBelowAverageNumber, actualBelowAverageNumber);
+    }
+
+    @Test
+    public void shouldFindAboveAverageNumber() {
+        SalesCalculatorService service = new SalesCalculatorService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedAboveAverageNumber = 5;
+        int actualAboveAverageNumber = service.getMonthAboveAverage(sales);
+
+        Assertions.assertEquals(expectedAboveAverageNumber, actualAboveAverageNumber);
+
+    }
+
+}
